bb5e67a9eb2526e6e62addf38e3f6196d72fbdd6
Barshon <barshonweb@gmail.com>
2024-06-09 09:50:21 +0600

    server ready to run

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express=require('express');
+const cors=require('cors');
+const http=require('http');
+const {MongoClient}=require('mongodb');
+const socketIo=require('socket.io');
+const port=process.env.PORT||5000;
+
+const app=express();
+app.use(cors());
+const server=http.createServer(app);
+
+
+server.listen(port,()=>{
+    console.log(`server running on port: ${port}`);
+})
