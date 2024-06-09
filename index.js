3c5c3abe7226a7eafdbad2fb015916eff8e5e1ab
Barshon <barshonweb@gmail.com>
2024-06-09 13:07:28 +0600

    chat app server made

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const cors = require("cors");
 const http = require("http");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+app.use(cors());
+
 const socketIo = require("socket.io");
 const port = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
 const server = http.createServer(app);
+const io=socketIo(server,{
+    cors: {
+      origin: "http://localhost:5173"
+    }
+  });
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_CLUSTER_URL}`;
-
 const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -18,15 +23,42 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
-async function run() {
+
+   function run() {
     try {
- 
-      await client.db("admin").command({ ping: 1 });
+       
+
+    const chatDB=client.db('chatDb');
+    const chatCollection= chatDB.collection('chatCollection');
+
+    io.on('connection',(socket)=>{
+        console.log('a user connected');
+
+       chatCollection.find({}).toArray()
+       .then(messages=>socket.emit('initialchats',messages));
+        
+        socket.on('message',(data)=>{
+            const message={
+                chat:data
+            };
+
+            chatCollection.insertOne(message) 
+                    io.emit('message',message);
+                
+            
+        })
+
+        socket.on('disconnect',()=>{
+            console.log('user disconnected');
+        })
+    })
+
+       client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
     }
   }
-    run().catch(console.dir);
+    run();
 server.listen(port, () => {
   console.log(`server running on port: ${port}`);
 });
